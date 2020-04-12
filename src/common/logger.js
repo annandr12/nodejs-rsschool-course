bf2cefd396f9c2634f877c6b800f6d84117cfd39
bannandr <annandr12@gmail.com>
2020-04-12 22:12:03 +0200

    add winston logger

--- src/common/logger.js
+++ src/common/logger.js
@@ -1,6 +1,29 @@
+const path = require('path');
+const fs = require('fs');
+const LOG_DIR = 'logs';
+const winston = require('winston');
+
+const dir = path.join(LOG_DIR);
+
+// eslint-disable-next-line no-sync
+if (!fs.existsSync(dir)) {
+  // eslint-disable-next-line no-sync
+  fs.mkdirSync(dir);
+}
+
+const logger = winston.createLogger({
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: path.join(dir, 'app_log.log') })
+  ]
+});
+
 module.exports = (req, res, next) => {
-  console.log('originalUrl:', req.originalUrl);
-  console.log('query:', req.query);
-  console.log('body:', req.body);
+  logger.log({
+    level: 'info',
+    message: `originalUrl: ${req.originalUrl} query: ${JSON.stringify(
+      req.query
+    )} body: ${JSON.stringify(req.body)}`
+  });
   next();
 };
